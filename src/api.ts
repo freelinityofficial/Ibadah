2084600978f89047654bc0e2bc884e7da92c0bc5
Freelinity <freelinityofficial@gmail.com>
2025-09-29 22:45:43 +0300

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // src/services/api.ts
-export const BASE_URL = "http://192.168.1.9:8000/api";
+export const BASE_URL = "http://api.ibadah.xyz/api";
 
 // ========== AUTH ========== //
 export async function login(username: string, password: string) {
